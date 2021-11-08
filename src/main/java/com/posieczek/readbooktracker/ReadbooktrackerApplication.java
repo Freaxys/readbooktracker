d5e281d07a48c5993fd37b0abc1dc859a839abf7
Michał Posieczek <micposi90@gmail.com>
2021-11-08 18:54:12 +0100

    Initial commit

---
+++ src/main/java/com/posieczek/readbooktracker/ReadbooktrackerApplication.java
@@ -0,0 +1,13 @@
+package com.posieczek.readbooktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadbooktrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadbooktrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/posieczek/readbooktracker/ReadbooktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posieczek.readbooktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadbooktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
